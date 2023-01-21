2a796af6238af3d6ff40140f2c2c98bf9f9fd621
Jerin Nadar <jerinnadar7@student.sfit.ac.in>
2023-01-21 12:40:00 +0530

    first_commit

---
+++ src/pages/Auth/AboutAuth.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const AboutAuth = () => {
+  return (
+    <div className="auth-container-1">
+        <h1>Join the Stack Overflow comunity</h1>
+        <p>Get unstuck-ask a question</p>
+        <p >Unlock your privilages like voting and commenting</p>
+        <p>Save your favorite tags,filters,and jobs</p>
+        <p> Earn reputation and badges</p>
+        <p style={{fontSize:'13px',color:'#666767'}}>collabrate and share knowlege with a private group for</p>
+       <p style={{fontSize:'13px',color:'#007acb'}}>  Get Stack Overflow for Tems free for up to 50 users</p>      
+    </div>
+  )
+}
+
+export default AboutAuth
